ffe7c895fd41b117ed683ff80543a9911716c4c2
Laurie Lonecrow <laurieteresa@gmail.com>
2021-12-05 15:59:12 -0500

    fix: add Head with font info, fix modal hover color

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 //components
 import Layout from '../layouts/LayoutMain'
+import Head from 'next/head'
 
 //styles
 import '../styles/globals.css'
@@ -8,6 +9,12 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Head>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
   <Layout>
   <Component {...pageProps} />
   </Layout>
