4e7cf4b42e7eca35d3b8283cc1e9e0b4b2c8b029
Laurie Lonecrow <laurieteresa@gmail.com>
2021-12-06 11:18:12 -0500

    feat: add instagram link

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -12,7 +12,7 @@ export default function Footer () {
         <h2 className={styles.footerText}>{`Booking through Instagram DM @elysiahoney`}</h2>
         <ul className={styles.list}>
             <li className={styles.link}>
-            <Link href='/' passHref><BsInstagram/></Link>
+            <Link href='https://www.instagram.com/elysiahoney/' passHref><BsInstagram/></Link>
             </li>
         </ul>
         </div>
