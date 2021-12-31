4ad68c87a0db23592dc17f19bbe09a23049e5f34
Laurie Lonecrow <laurieteresa@gmail.com>
2021-12-31 14:29:28 -0500

    fix: update image urls

--- components/ModalNav/index.js
+++ components/ModalNav/index.js
@@ -1,28 +1,17 @@
 import Link from 'next/link';
-
 //styles
 import styles from './Modal.module.css';
 
-
 export default function ModalNav() {
-
-
-    return (
+  return (
     <div className={styles.mobileMenu}>
-        <nav>
-          <ul className={styles.mobileNav}>
-            <li className={styles.mobileLink}>
-            <Link href='/about'>About</Link>
-            </li>
-            <li className={styles.mobileLink}>
-            <Link href='/pricing'>Pricing</Link>
-            </li>
-            <li className={styles.mobileLink}>
-            <Link href='/story'>My Story</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <nav>
+        <ul className={styles.mobileNav}>
+          <li className={styles.mobileLink}><Link href='/about'>About</Link></li>
+          <li className={styles.mobileLink}><Link href='/pricing'>Pricing</Link></li>
+          <li className={styles.mobileLink}><Link href='/story'>My Story</Link></li>
+        </ul>
+      </nav>
     </div>
-    )
-}
+  );
+};
