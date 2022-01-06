e93ffe5d7346f74f870ae3a7ffddac7df9f2bbe3
Laurie Lonecrow <laurieteresa@gmail.com>
2022-01-06 16:51:18 -0500

    feat: add polaroid images to footer

--- components/ImageSlideShow/index.js
+++ components/ImageSlideShow/index.js
@@ -10,7 +10,7 @@ export default function ImageSlideShow() {
     return (
         <div className={styles.slide_show}>
         {size.width < 576 ? (polaroids.slice(0,3).map(pic => (
-          <Polaroid src={pic.src}/>))) 
+          <Polaroid id={id} src={pic.src}/>))) 
           : polaroids.map(pic => (<Polaroid src={pic.src}/>))}
         </div>
     )
