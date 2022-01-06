9e97ae974ecb885bd813b2fb4a56ee4286e35c37
Laurie Lonecrow <laurieteresa@gmail.com>
2022-01-06 12:05:27 -0500

    feat: windowSize hook to add mobile footer polariod images

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,24 +1,23 @@
 //components
-import Link from 'next/link'
 
 //styles
 import {BsInstagram} from 'react-icons/bs'
 import ImageSlideShow from '../ImageSlideShow'
 import styles from './Footer.module.css'
 
-export default function Footer () {
+ const Footer = () => (
 
-    return (
         <div className={styles.wrapper}> 
           <h2 className={styles.footerText}>{`Booking through Instagram DM @elysiahoney --- 
           Booking through Instagram DM @elysiahoney ---`}</h2>
           <ul className={styles.list}>
             <li className={styles.link}>
-            <Link href='https://www.instagram.com/elysiahoney/' passHref><BsInstagram/></Link>
+            <a href='https://www.instagram.com/elysiahoney/'><BsInstagram/></a>
             </li>
           </ul>
           <ImageSlideShow/>
         </div>
 
     )
-}
+
+export default Footer
--- components/ImageSlideShow/index.js
+++ components/ImageSlideShow/index.js
@@ -1,13 +1,17 @@
+
 import Polaroid from "./Polaroid";
 import styles from './Image.module.css'
 import { polaroids } from "../../constants/constants";
+import useWindowSize from "../../hooks/useWindowSize";
+
 
 export default function ImageSlideShow() {
+    const size = useWindowSize();
     return (
         <div className={styles.slide_show}>
-        {polaroids.map(pic => (
-            <Polaroid src={pic.src}/>
-        ))}
+        {size.width < 576 ? (polaroids.slice(0,3).map(pic => (
+          <Polaroid src={pic.src}/>))) 
+          : polaroids.map(pic => (<Polaroid src={pic.src}/>))}
         </div>
     )
   }
