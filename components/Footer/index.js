a789574a51a20df9d85f8bf4c219a43464bb6cd2
Laurie Lonecrow <laurieteresa@gmail.com>
2022-01-06 19:43:08 -0500

    fix: adjust footer margins/spacing

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,13 +8,11 @@ import styles from './Footer.module.css'
  const Footer = () => (
 
         <div className={styles.wrapper}> 
+          <div className={styles.list}>
+            <a className={styles.link} href='https://www.instagram.com/elysiahoney/'><BsInstagram/></a>
+          </div>
           <h2 className={styles.footerText}>{`Booking through Instagram DM @elysiahoney --- 
           Booking through Instagram DM @elysiahoney ---`}</h2>
-          <ul className={styles.list}>
-            <li className={styles.link}>
-            <a href='https://www.instagram.com/elysiahoney/'><BsInstagram/></a>
-            </li>
-          </ul>
           <ImageSlideShow/>
         </div>
 
